209c45647ecf2aa69a9fd9e97c77c16c4e1b4782
adamo <adamo@DESKTOP-HLAB05J>
2018-07-14 23:26:36 -0400

    time adjustment

--- jbot-example/src/main/java/com/adamoutler/time/CityKingsTime.java
+++ jbot-example/src/main/java/com/adamoutler/time/CityKingsTime.java
@@ -15,7 +15,7 @@ import java.util.Date;
  */
 public class CityKingsTime {
 
-    final static int CKSTOPTIME = 4;   //UTC hour 2 = 0300 UK, 2400 EST, 2100 PAC, 1200 BEJ
+    final static int CKSTOPTIME = 3;   //UTC hour 2 = 0300 UK, 2400 EST, 2100 PAC, 1200 BEJ
     final static int CKSTARTTIME = 8;  //UTC hour 7 = 0800 UK, 0400 EST, 2300 PAC, 1800 BEJ
 
 //    public static void main(String[] args) {
